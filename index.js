c861cb2502deb22cee2ffc78844a848bb6fc0c8c
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-16 08:35:44 -0700

    Reformat with Prettier

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-
-
 const mostFrequentAppearance = array => {
-let mostFrequent = 1;
-let occurrence = 0;
-let item;
-for (let i = 0; i < array.length; i++) {
-for (let j = i; j < array.length; j++) {
-if (array[i] === array[j]) {
-occurrence++;}
- if (mostFrequent < occurrence) {
-    mostFrequent = occurrence;
-item = array[i];
-                                }
+    let mostFrequent = 1;
+    let occurrence = 0;
+    let item;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i; j < array.length; j++) {
+            if (array[i] === array[j]) {
+                occurrence++;
+            }
+            if (mostFrequent < occurrence) {
+                mostFrequent = occurrence;
+                item = array[i];
+            }
         }
         occurrence = 0;
     }
-return item;
+    return item;
 }
-
-console.log(mostFrequentAppearance([1, 2, 3, 3, 3, 4]))
-
-
-
